28218a9c7137c808b72cfa28fb45abd4c4f44c1e
kanetch <kanetch@example.com>
2021-01-17 00:50:23 +0900

    Use React Hooks

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,49 +1,33 @@
-import React from 'react'
+import { useState } from 'react'
 
-class LoginForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.handleChange = this.handleChange.bind(this)
-    this.state = {
-      id: '',
-      password: '',
-    }
-  }
+function LoginForm(props) {
+  const [id, setId] = useState('')
+  const [password, setPassword] = useState('')
 
-  handleChange(e) {
-    const name = e.target.name
-    const value = e.target.value
-    this.setState({
-      [name]: value,
-    })
-  }
-
-  render() {
-    return (
-      <form className='login-form'>
-        <fieldset>
-          <div className='user-input'>
-            <label for='id'>ID</label>
-            <input
-              name='id'
-              type='text'
-              value={this.state.id}
-              onChange={this.handleChange}
-            ></input>
-          </div>
-          <div className='user-input'>
-            <label for='password'>Password</label>
-            <input
-              name='password'
-              type='password'
-              value={this.state.password}
-              onChange={this.handleChange}
-            ></input>
-          </div>
-        </fieldset>
-      </form>
-    )
-  }
+  return (
+    <form className='login-form'>
+      <fieldset>
+        <div className='user-input'>
+          <label for='id'>ID</label>
+          <input
+            name='id'
+            type='text'
+            value={id}
+            onChange={e => setId(e.target.value)}
+          ></input>
+        </div>
+        <div className='user-input'>
+          <label for='password'>Password</label>
+          <input
+            name='password'
+            type='password'
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          ></input>
+        </div>
+      </fieldset>
+    </form>
+  )
 }
 
 export default LoginForm
