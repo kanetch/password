bf4c1bb8c5ad30131ee4bbf7cc042ff85acb5c3e
kanetch <kanetch@example.com>
2022-07-10 04:55:25 +0900

    rebuild from scratch

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import LoginForm from './LoginForm'
+
+function App() {
+  return (
+    <div className='text-center'>
+      <p className='py-2'>
+        ブラウザにパスワードを自動生成させるためのダミーページです。
+      </p>
+      <LoginForm></LoginForm>
+    </div>
+  )
+}
+
+export default App
---
+++ src/LoginForm.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+function LoginForm() {
+  const [id, setId] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <div className='flex justify-center'>
+      <form className='w-full max-w-sm px-2'>
+        <div className='py-2'>
+          <label>
+            <span className='block text-left'>ID</span>
+            <input
+              className='w-full border-b-2 border-blue-300 bg-gray-100 px-2 py-2 focus:bg-white focus:outline focus:outline-2 focus:outline-offset-1 focus:outline-blue-200'
+              type='text'
+              name='id'
+              value={id}
+              required
+              onChange={e => setId(e.target.value)}
+            ></input>
+          </label>
+        </div>
+        <div className='py-2'>
+          <label>
+            <span className='block text-left'>Password</span>
+            <input
+              className='w-full border-b-2 border-blue-300 bg-gray-100 px-2 py-2 focus:bg-white focus:outline focus:outline-2 focus:outline-offset-1 focus:outline-blue-200'
+              type='password'
+              name='password'
+              value={password}
+              required
+              onChange={e => setPassword(e.target.value)}
+            ></input>
+          </label>
+        </div>
+        <div className='py-4'>
+          <input
+            className='w-full bg-gray-300 py-2 font-semibold text-white'
+            type='submit'
+            value='LOGIN'
+            disabled
+          ></input>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default LoginForm
